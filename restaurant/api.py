4ed02dd4bd791e6efda42c026b19c36bffeb0a3d
Adel Narod <adelnarod@gmail.com>
2022-09-15 09:54:16 +0400

    little fixes

--- restaurant/api.py
+++ restaurant/api.py
@@ -1,21 +1,28 @@
 from ninja import Router
 from .models import Restaurant
-from .serializers import RestaurantIn
+from .serializers import RestaurantIn, RestaurantOut, Message
 from typing import List
 from django.shortcuts import get_object_or_404
 
+
 router = Router()
 
-@router.post("/", response=RestaurantIn)
+@router.post("/", response={200: Message, 400: None})
 def create_restaurant(request, payload: RestaurantIn):
-    restaurant = Restaurant.objects.create(**payload.dict())
-    return restaurant
+    restaurant = Restaurant(**payload.dict())
+    restaurant.save()
+    return 200, { "message": "Successfully Created" }
 
-@router.get("/", response=List[RestaurantIn])
-def list(request):
-    return Restaurant.objects.all()
 
-@router.get('/{restaurant_id}/', response=RestaurantIn)
+@router.get("/", response=List[RestaurantOut])
+def list(request, search_query: str = ""):
+    if search_query:
+        restaurant = Restaurant.objects.filter(search_terms__icontains=search_query)
+        return restaurant
+    else:
+        return Restaurant.objects.all()
+
+@router.get('/{restaurant_id}/', response=RestaurantOut)
 def get_restaurant(request, restaurant_id: int):
     restaurant = get_object_or_404(Restaurant, id=restaurant_id)
     return restaurant
@@ -28,13 +35,8 @@ def update_restaurant(request, payload: RestaurantIn, restaurant_id: int):
     restaurant.save()
     return restaurant
 
-@router.delete('/{restaurant_id}/')
+@router.delete('/{restaurant_id}/', response={200: Message, 400: None})
 def delete_restaurant(request, restaurant_id: int):
     restaurant = get_object_or_404(Restaurant, id=restaurant_id)
     restaurant.delete()
-    return { "successfully_deleted" : True }
-
-@router.get('/search', response=List[RestaurantIn])
-def search_restaurant(request, search_query: str):
-    restaurant = Restaurant.objects.filter(search_terms__icontains=search_query)
-    return restaurant
+    return 200, { "message": "Delete Successfull"}
--- restaurant/serializers.py
+++ restaurant/serializers.py
@@ -5,3 +5,11 @@ class RestaurantIn(Schema):
     name: str
     restaurant_type: str
     rating: int = None
+
+class RestaurantOut(Schema):
+    name: str
+    restaurant_type: str
+    rating: int
+
+class Message(Schema):
+    message: str
